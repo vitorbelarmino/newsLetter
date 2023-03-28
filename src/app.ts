446ef2a8c642a53bfc103e24b7028627d85b2af1
vitor <vitor.belarmino@hotmail.com>
2023-03-28 00:52:52 -0300

    feat: :sparkles: add middleware de error e error customizado

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
+import 'express-async-errors';
 import express from 'express';
 import 'dotenv/config';
 import { router } from './routes';
+import globalError from './middleware/GlobalError';
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(globalError);
 
 export { app };
